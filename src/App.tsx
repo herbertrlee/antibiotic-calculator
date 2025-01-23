6a15c8f9ed03dcfff0f37bd9f36437ea0a3387d6
Herbert Lee <herbert.lee@striiv.com>
2025-01-22 23:42:43 -0800

    Add antibiotic selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,45 @@
 import {useCallback, useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {createClient} from "@supabase/supabase-js";
+import Select from "react-select";
 
 
 const supabase = createClient("https://ipwagddgrsuwbwxdntpg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwd2FnZGRncnN1d2J3eGRudHBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2MDk3NDcsImV4cCI6MjA1MzE4NTc0N30.Ukpeh8P9Ap2d43pRiiSgCwqTZSi4dEP1ebRm7IinLI8")
 
 
-function App() {
-  const [count, setCount] = useState(0)
-    const [regimens, setRegimens] = useState([])
+type Option = {
+    label: string
+    value: number
+}
 
-    const fetchRegimens = useCallback(async () => {
-        const { data } = await supabase.from("regimen").select()
-        console.log(data)
-    }, [setRegimens])
+function App() {
+    const [antibioticOptions, setAntibioticOptions] = useState<Option[]>([])
+    const [selectedAntibioticId, setSelectedAntibioticId] = useState<number | null>(null)
+
+    const fetchAntibiotics = useCallback(async () => {
+        const {data} = await supabase.from("antibiotic").select()
+        setAntibioticOptions(data?.map((antibiotic) => {
+            return {label: antibiotic.name, value: antibiotic.id}
+        }) ?? [])
+    }, [setAntibioticOptions])
 
     useEffect(() => {
-        fetchRegimens()
-    }, [fetchRegimens, regimens]);
+        fetchAntibiotics()
+    }, [fetchAntibiotics])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <Select options={antibioticOptions}
+                        onChange={(newValue) => {
+                            if (!!newValue) {
+                                setSelectedAntibioticId(newValue.value)
+                            }
+                        }}
+                />
+            </div>
+        </>
+    )
 }
 
 export default App
