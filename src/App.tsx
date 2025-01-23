2b8732e1741beba0e9c91a08db07bdde1553d0e3
Herbert Lee <herbert.lee@striiv.com>
2025-01-22 23:17:00 -0800

    Hit supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {createClient} from "@supabase/supabase-js";
+
+
+const supabase = createClient("https://ipwagddgrsuwbwxdntpg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwd2FnZGRncnN1d2J3eGRudHBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2MDk3NDcsImV4cCI6MjA1MzE4NTc0N30.Ukpeh8P9Ap2d43pRiiSgCwqTZSi4dEP1ebRm7IinLI8")
+
 
 function App() {
   const [count, setCount] = useState(0)
+    const [regimens, setRegimens] = useState([])
+
+    const fetchRegimens = useCallback(async () => {
+        const { data } = await supabase.from("regimen").select()
+        console.log(data)
+    }, [setRegimens])
+
+    useEffect(() => {
+        fetchRegimens()
+    }, [fetchRegimens, regimens]);
 
   return (
     <>
